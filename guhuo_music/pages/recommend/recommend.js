ae189d75fa6d9a1a639dcd5dfe14859cc0075593
zrc1708 <1023054043@qq.com>
2021-01-21 23:25:03 +0800

    更新

---
+++ guhuo_music/pages/recommend/recommend.js
@@ -0,0 +1,50 @@
+import request from '../../utils/request'
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    day: '', // 天
+    month: '', // 月
+    recommendList: [], // 推荐列表数据
+    index: 0, // 点击音乐的下标
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 判断用户是否登录
+    let userInfo = wx.getStorageSync('userInfo');
+    if(!userInfo){
+      wx.showToast({
+        title: '请先登录',
+        icon: 'none',
+        success: () => {
+          // 跳转至登录界面
+          wx.reLaunch({
+            url: '/pages/login/login'
+          })
+        }
+      })
+    }
+    // 更新日期的状态数据
+    this.setData({
+      day: new Date().getDate(),
+      month: new Date().getMonth() + 1
+    })
+
+    // 获取每日推荐的数据
+    this.getRecommendList();
+  },
+
+  // 获取用户每日推荐数据
+  async getRecommendList(){
+    let recommendListData = await request('/recommend/songs');
+    this.setData({
+      recommendList: recommendListData.data.dailySongs
+    })
+  },
+})
---
+++ guhuo_music/pages/songDetail/songDetail.js
@@ -0,0 +1,72 @@
+// pages/songDetail/songDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isPlay: false, // 音乐是否播放
+    song: {}, // 歌曲详情对象
+    musicId: '', // 音乐id
+    musicLink: '', // 音乐的链接
+    currentTime: '00:00',  // 实时时间
+    durationTime: '00:00', // 总时长
+    currentWidth: 0, // 实时进度条的宽度
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
